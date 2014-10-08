07538e08a1d4874d1b36445cb3fa07ad5ab17953
Mário Feroldi <thelost-t@live.com>
2014-10-08 15:06:41 -0300

    Update Translator.hs

--- src/Translator.hs
+++ src/Translator.hs
@@ -12,6 +12,7 @@ import Parser
 data Inst
   = PushVar String
   | PushConst String
+  | PushString String
   | AssignTo Inst Inst
   | Operation String Inst Inst
   | EachTo Inst Inst
@@ -20,12 +21,14 @@ data Inst
   | Block Inst
   | MakeCondition Inst Inst Inst
   | Function Inst [Inst] Inst     -- auto foo = [](auto a, auto b){ return a + b; }
-  | Lambda  [Inst] Inst           -- [](auto a, auto b){ }
+  | Lambda [Inst] Inst            -- [](auto a, auto b){ }
   | CallFunction Inst [Inst]      -- foo (1, x, "Hey")
+  | Error String                  -- For format errors
   deriving (Show, Read, Eq)
 
 
 parseAst (Ident x) = PushVar x
+parseAst (CharString x) = PushString x
 parseAst (Num x) = PushConst (show x)
 parseAst (Assign e1 e2) = AssignTo (parseAst e1) (parseAst e2)
 
@@ -43,9 +46,92 @@ parseAst (Le  e1 e2) = Operation "<=" (parseAst e1) (parseAst e2)
 
 parseAst (Each e1 e2) = EachTo (parseAst e1) (parseAst e2)
 parseAst (ComprehensionList [CountList e1 e2]) = MakeCountList (parseAst e1) (parseAst e2)
+
+-- Countlist error
+parseAst (ComprehensionList ((CountList e1 e2):rest))
+  = Error $
+      "\nHey! Lists cannot has two or more countlists:\n\t["
+      ++ (show $ parseAst e1)
+      ++ ".."
+      ++ (show $ parseAst e2)
+      ++ ", ..]\n"
+
 parseAst (ComprehensionList xs) = MakeSimpleList (map parseAst xs)
 parseAst (ParenthesesBlock e1) = Block (parseAst e1)
 parseAst (Def name args body) = Function (parseAst name) (map parseAst args) (parseAst body)
 parseAst (LambdaDef args body) = Lambda (map parseAst args) (parseAst body)
 parseAst (Call name args) = CallFunction (parseAst name) (map parseAst args)
 parseAst (Condition stat thenStat elseStat) = MakeCondition (parseAst stat) (parseAst thenStat) (parseAst elseStat)
+
+
+-- To translate tokens to instructions, you should do this:
+--
+-- execute $ tokenise "def foo a = a * 2 and print with foo in 5"
+
+type Stack = [Token]
+
+execute :: Stack -> [String]
+execute stack
+  = let
+      paragraphs :: [Ast]
+      paragraphs = parseEofs stack
+
+      outcode = map parseAst paragraphs
+
+    in
+      map (\x -> (translate x) ++ ";") outcode
+
+translate :: Inst -> String
+translate inst
+  = case inst of
+      PushVar x ->
+        x
+
+      PushConst x ->
+        x
+
+      PushString x ->
+        "\"" ++ x ++ "\""
+
+      AssignTo i1 i2 ->
+        (translate i1) ++ " = " ++ (translate i2)
+
+      Operation op i1 i2 ->
+        (translate i1) ++ op ++ (translate i2)
+
+      EachTo lambda list ->
+        "eachlist(" ++ (translate lambda) ++ ", " ++ (translate list) ++ ")"
+
+      MakeCountList min_ max_ ->
+        "countlist(" ++ (translate min_) ++ ", " ++ (translate max_) ++ ")"
+
+      MakeSimpleList content ->
+        "{" ++ (intercalate "," $ map translate content) ++ "}"
+
+      Block i ->
+        "(" ++ (translate i) ++ ")"
+
+      --"if(" ++ (translate statif) ++ "){" ++ (translate statthen) ++ ";}else{" ++ (translate statelse) ++ ";};"
+      MakeCondition statif statthen statelse ->
+        "condition(" ++ (translate statif) ++ "," ++ (translate statthen) ++ "," ++ (translate statelse) ++ ")"
+
+      Function name args body ->
+        let
+          strname = translate name
+        in
+          if strname /= "main"
+            then
+              "auto " ++ (translate name) ++ " = [](" ++ (intercalate "," $ map (\x -> "auto "++(translate x)) args) ++ "){ return " ++ (translate body) ++ "; }"
+
+            else
+              "int main( int argc, char** argv){ return " ++ (translate body) ++ "; }"
+
+      Lambda args body ->
+        "[](" ++ (intercalate "," $ map (\x -> "auto "++(translate x)) args) ++ "){ return " ++ (translate body) ++ "; }"
+
+      CallFunction name args ->
+        (translate name) ++ "(" ++ (intercalate "," $ map translate args) ++ ")"
+
+      Error msg ->
+        error msg
+
