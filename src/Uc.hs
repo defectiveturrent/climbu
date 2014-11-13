858653ae464e9d27c8532bb2f40421b1aa7d57fe
Mário Feroldi <thelost-t@live.com>
2014-11-13 16:17:17 -0200

    Update Uc.hs

--- src/Uc.hs
+++ src/Uc.hs
@@ -66,7 +66,7 @@ interpret (lines:_)
 
 version _
   = do
-      putStrLn "Climbu compiler v0.6 - Copyright (C) 2014  Mario Feroldi"
+      putStrLn "Climbu compiler v1.0 - Copyright (C) 2014  Mario Feroldi"
       putStrLn "This program comes with ABSOLUTELY NO WARRANTY."
       putStrLn "This is free software, and you are welcome to redistribute it"
       putStrLn "under GPL v3 license.\n"
