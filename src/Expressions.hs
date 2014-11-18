2054cc30f77a7a6cf2322f3b64d7513f9e367d76
Mário Feroldi <thelost-t@live.com>
2014-11-18 14:56:56 -0200

    Update Expressions.hs

--- src/Expressions.hs
+++ src/Expressions.hs
@@ -1,3 +1,21 @@
+{-
+    Climbu compiler / interpreter
+    Copyright (C) 2014  Mario Feroldi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Expressions where
 
 import Data.Char
@@ -12,6 +30,7 @@ type Asts = [Ast]
 data Token
   = CONST Int
   | CONSTF Float
+  | VAR
   | ID String
   | STRING String
   | CHAR Char
@@ -71,6 +90,7 @@ data Token
 data Ast
   = Def Ast [Ast] Ast                  -- ID [ARGS] BODY
   | Assign Ast Ast                     -- Assign AST AST
+  | Decl String                        -- var foo
   | Add Ast Ast                        -- Plus AST AST
   | Sub Ast Ast                        -- Minus AST AST
   | Mul Ast Ast                        -- Times AST AST
@@ -130,6 +150,7 @@ eofers
     , CLOSEKEYS
     , RARROW
     , LARROW
+    -- , VAR
     , IF
     , THEN
     , ELSE
