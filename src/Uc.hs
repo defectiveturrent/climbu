fd6a51c015bb0bf53ff25b7bb10a262bb7e56b01
Mário Feroldi <thelost-t@live.com>
2014-11-18 14:57:47 -0200

    Update Uc.hs

--- src/Uc.hs
+++ src/Uc.hs
@@ -1,3 +1,21 @@
+{-
+    Climbu compiler / interpreter
+    Copyright (C) 2014  Mario Feroldi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Main where
 
 import System.Environment
@@ -18,7 +36,7 @@ catch = catchIOError
 toTry :: IO ()
 toTry = do
     (command:args) <- getArgs
-    let (Just action) = lookup command dispatch in action args -- get the function that corresponds to argument
+    let (Just action) = lookup command dispatch in action args -- gets the function that corresponds to argument
 
 handler :: IOError -> IO ()  
 handler e
@@ -66,7 +84,7 @@ interpret (lines:_)
 
 version _
   = do
-      putStrLn "Climbu compiler v1.0 - Copyright (C) 2014  Mario Feroldi"
+      putStrLn "Climbu compiler v1.1 - Copyright (C) 2014  Mario Feroldi"
       putStrLn "This program comes with ABSOLUTELY NO WARRANTY."
       putStrLn "This is free software, and you are welcome to redistribute it"
       putStrLn "under GPL v3 license.\n"
