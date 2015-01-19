e7c94e92309b0a42975228d12a807bfc0d7dca5a
Mário Feroldi <thelost-t@live.com>
2015-01-19 17:44:00 -0200

    changed licence

--- src/Uc.hs
+++ src/Uc.hs
@@ -1,19 +1,25 @@
 {-
-    Climbu compiler / interpreter
-    Copyright (C) 2014 - 2015 Mario Feroldi
+    The MIT License (MIT)
+    
+    Copyright (c) 2015 Mário Feroldi Filho
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 -}
 
 module Main where
@@ -97,10 +103,8 @@ interpret (line:_)
 
 version _
   = do
-      putStrLn "Climbu compiler v1.8 (Actors) - Copyright (C) 2014 - 2015  Mario Feroldi"
-      putStrLn "This program comes with ABSOLUTELY NO WARRANTY."
-      putStrLn "This is free software, and you are welcome to redistribute it"
-      putStrLn "under GPL v3 license.\n"
+      putStrLn "Climbu v1.8 (Actors) - The MIT License (MIT)"
+      putStrLn "Copyright (c) 2015 Mário Feroldi Filho"
 
 help _
   = do
